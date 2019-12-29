9e220c2b21874ae6a99a215e60d819c543df4c94
stevecward <steve.coward@gmail.com>
2019-12-29 18:55:51 -0500

    adds driver for CreateScheduledTask task

---
+++ CreateScheduledTask/CreateScheduledTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SharpTasks.Execution;
+
+namespace CreateScheduledTask
+{
+    public static class CreateScheduledTask
+    {
+        static void Main(string[] args)
+        {
+            string output = ScheduledTask.CreateScheduledTask("hourly", "3", "\\Windows\\MSEdge\\Microsoft Edge Update Tool", "C:\\Windows\\System32\\calc.exe");
+            Console.WriteLine(output);
+            Console.ReadKey();
+        }
+    }
+}
