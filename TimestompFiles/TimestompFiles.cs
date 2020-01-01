dc3f6493a4b3ba19f5ac592eb3f7331c9e45072e
stevecward <steve.coward@gmail.com>
2020-01-01 13:30:43 -0500

    adds simple driver for demonstrating the Timestomp SharpTasks library.

---
+++ TimestompFiles/TimestompFiles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SharpTasks.Evasion;
+
+namespace TimestompFiles
+{
+    public static class Task
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Timestomp.SetFileTimestamp(args[0], args[1], args[2]));
+            Console.ReadKey();
+        }
+    }
+}
